d64e1f533b542bb30e3afd55ea022fef2347f34d
PipitoneSamuele <samuele.pipitone@gmail.com>
2022-10-01 17:43:08 +0200

    Server created

---
+++ CoCoin_server/src/main/java/com/app/CoCoin_server/CoCoinServerApplication.java
@@ -0,0 +1,13 @@
+package com.app.CoCoin_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoCoinServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoCoinServerApplication.class, args);
+	}
+
+}
